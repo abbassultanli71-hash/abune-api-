e1b6aee61eaff1e40d776e09a11d078bee0c9033
Abbas Sultanli <abbassultanli71@gmail.com>
2026-07-30 22:08:44 +0400

    fix(cache): fix doctype syntax and bump ServiceWorker cache to force immediate client cache update to mobile app

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // ─── Service Worker: Abunəm Instant Auto-Update Engine (v87000.0.0) ───
 
-const CACHE_VERSION = 'abune-v87000.0.11';
+const CACHE_VERSION = 'abune-v99999.0.0';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
