664eb4f52d5ce9fec40792d660fce26c947da545
Abbas Sultanli <abbassultanli71@gmail.com>
2026-07-30 22:54:39 +0400

    fix(download): link APK download buttons directly to 100% working Catbox CDN direct download URL

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/abunem.apk', (req, res) => {
-  res.redirect('https://github.com/abbassultanli71-hash/abune-api-/releases/download/v2.0.0/abunem.apk');
+  res.redirect('https://files.catbox.moe/6rxuxm.apk');
 });
 
 const jwt = require('jsonwebtoken');
